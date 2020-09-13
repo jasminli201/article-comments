630c1ca1e2da13012afe5a84002dd68a5c2c98ec
Jasmin Li <jl6ww@virginia.edu>
2020-09-13 15:54:49 -0400

    added firebase database, configured express server for firebase, connected react client to read sample firebase data

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,45 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [hello, setHello] = useState([]);
+  // useState is a hook that returns a pair: current state value and a function to update state
+  // arg to useState is the initial state
+  const [comments, setComments] = useState([]);
 
-  async function fetchData() {
-    let response = await fetch("http://localhost:5000/hello");
+  async function fetchComments() {
+    let response = await fetch("http://localhost:5000/readAllComments");
     response.json()
-      .then(response => setHello(response));
+      .then(response => {
+        // map through object of objects to turn it into array of objects
+        var commentsArray = Object.keys(response).map(key => {
+          return response[key];
+        })
+        setComments(commentsArray);
+      });
   }
 
+  // useEffect adds ability to perform side effects from a function component
+  // same as componentDidMount, update and unmount -> into one API
+  // runs after flushing changes to the DOM
   useEffect(() => {
-    fetchData();
-  });
+    fetchComments();
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        {hello.hello}
+        {/* map through array of objects to display each comment's content */}
+        {comments.map(comment => {
+          return (
+            <div>
+              <p>Time: {comment.time}</p>
+              <p>Content: {comment.content}</p>
+              <p>Likes: {comment.likes}</p>
+              <p>Dislikes: {comment.dislikes}</p>
+              <p>Replies: {comment.replies}</p>
+            </div>
+          )
+        })}
       </header>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 const express = require("express");
-const axios = require("axios");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 
-app.get("/hello", (req, res) => {
-    res.send({ "hello": "hello world" })
-});
+const firebase = require("firebase");
+const firebaseConfig = require("./firebase.json");
+firebase.initializeApp(firebaseConfig);
+
+app.get("/readAllComments", (req, res) => {
+    var comments = firebase.database().ref('comments');
+    comments.on("value", snapshot => {
+        res.send(snapshot.val())
+    });
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
